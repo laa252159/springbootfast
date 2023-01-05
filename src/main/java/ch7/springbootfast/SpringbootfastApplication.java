a464b1673c5307e9d718857d21d62a63ce2ad491
sanix <laa252159@gmail.com>
2023-01-05 11:55:27 +0400

    Initial commit

---
+++ src/main/java/ch7/springbootfast/SpringbootfastApplication.java
@@ -0,0 +1,13 @@
+package ch7.springbootfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootfastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch7/springbootfast/SpringbootfastApplicationTests.java
@@ -0,0 +1,13 @@
+package ch7.springbootfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootfastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
