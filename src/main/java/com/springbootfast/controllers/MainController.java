b7e5dc7845daa049f325437e9bac0724534c482f
sanix <laa252159@gmail.com>
2023-01-06 23:33:57 +0400

    RequestParams

--- src/main/java/com/springbootfast/controllers/MainController.java
+++ src/main/java/com/springbootfast/controllers/MainController.java
@@ -3,14 +3,17 @@ package com.springbootfast.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String home(Model page) {
-        page.addAttribute("color", "red");
-        page.addAttribute("username", "Katy");
+    public String home(@RequestParam(required = false, defaultValue = "green") String color,
+                       @RequestParam(required = false, defaultValue = "Noname") String userName,
+                       Model page) {
+        page.addAttribute("color", color);
+        page.addAttribute("username", userName);
         return "home.html";
     }
 
