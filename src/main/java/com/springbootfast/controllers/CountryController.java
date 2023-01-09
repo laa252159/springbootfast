b5132fdd977247b53c7e9ba32df366991faa9cb5
sanix <laa252159@gmail.com>
2023-01-09 21:28:52 +0400

    ResponseEntity

--- src/main/java/com/springbootfast/controllers/CountryController.java
+++ src/main/java/com/springbootfast/controllers/CountryController.java
@@ -1,6 +1,7 @@
 package com.springbootfast.controllers;
 
 import com.springbootfast.model.Country;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,13 @@ import java.util.List;
 public class CountryController {
 
     @GetMapping("/france")
-    public Country france() {
-        return Country.of("France", 101);
+    public ResponseEntity<Country> france() {
+        return ResponseEntity
+                .status(202)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .header("favorite_food", "cheese and wine")
+                .body(Country.of("France", 101));
     }
 
     @GetMapping("/countries")
