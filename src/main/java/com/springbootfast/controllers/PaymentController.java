ce9883ff90eedb85d5bcce9bfa60d0f72ee940bc
sanix <laa252159@gmail.com>
2023-01-11 22:20:53 +0400

    payment controller

--- src/main/java/com/springbootfast/controllers/PaymentController.java
+++ src/main/java/com/springbootfast/controllers/PaymentController.java
@@ -8,10 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
+
+    private static final Logger logger = Logger.getLogger(PaymentController.class.getName());
+
     private final PaymentService paymentService;
 
     public PaymentController(PaymentService paymentService) {
@@ -19,13 +25,15 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
+    public ResponseEntity<?> makePayment(@RequestBody PaymentDetails paymentDetails) {
+        logger.info(String.valueOf(paymentDetails.getAmount()));
+
 //        try {
-            PaymentDetails paymentDetails =
-                    paymentService.processPayment();
-            return ResponseEntity
-                    .status(HttpStatus.ACCEPTED)
-                    .body(paymentDetails);
+//            PaymentDetails paymentDetails =
+//                    paymentService.processPayment();
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
 //        } catch (NotEnoughMoneyException e) {
 //            ErrorDetails errorDetails = new ErrorDetails();
 //            errorDetails.setMessage("Not enough money to make the payment.");
@@ -33,6 +41,6 @@ public class PaymentController {
 //                    .badRequest()
 //                    .body(errorDetails);
 //        }
-        }
+    }
 
 }
