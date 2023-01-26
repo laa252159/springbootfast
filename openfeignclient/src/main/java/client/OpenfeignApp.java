bbf3b8854b278ac8f17bf3c635a6576ec1d13445
sanix <laa252159@gmail.com>
2023-01-26 21:40:30 +0400

    build fix

--- openfeignclient/src/main/java/client/OpenfeignApp.java
+++ openfeignclient/src/main/java/client/OpenfeignApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class OpenfeignApp {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(OpenfeignApp.class, args);
         System.out.println("Hello world!");
     }
 }
