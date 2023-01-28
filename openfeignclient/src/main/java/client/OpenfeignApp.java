815e7dbfb8d700d88c8fdcaeb7028af3f1855e98
sanix <laa252159@gmail.com>
2023-01-28 11:58:35 +0400

    refactoring

--- openfeignclient/src/main/java/client/OpenfeignApp.java
+++ openfeignclient/src/main/java/client/OpenfeignApp.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpenfeignApp {
     public static void main(String[] args) {
         SpringApplication.run(OpenfeignApp.class, args);
-        System.out.println("Hello world!");
     }
 }
